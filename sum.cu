069c36d9bf5e06bc1880aee24e9f7ddea4ef216d
vitalspace <myglobalsounds@gmail.com>
2024-07-07 13:27:01 -0600

    sum example

---
+++ sum.cu
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+
+__global__ void add(int *a, int *b, int *c)
+{
+    *c = *a + *b;
+}
+
+int main()
+{
+    int h_a = 5;
+    int h_b = 7;
+    int h_c;
+
+    int *d_a, *d_b, *d_c;
+
+    cudaMalloc((void **)&d_a, sizeof(int));
+    cudaMalloc((void **)&d_b, sizeof(int));
+    cudaMalloc((void **)&d_c, sizeof(int));
+
+    cudaMemcpy(d_a, &h_a, sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, &h_b, sizeof(int), cudaMemcpyHostToDevice);
+
+    add<<<1, 1>>>(d_a, d_b, d_c);
+
+    cudaMemcpy(&h_c, d_c, sizeof(int), cudaMemcpyDeviceToHost);
+
+    printf("%d + %d = %d\n", h_a, h_b, h_c);
+
+    cudaFree(d_a);
+    cudaFree(d_a);
+    cudaFree(d_c);
+}
